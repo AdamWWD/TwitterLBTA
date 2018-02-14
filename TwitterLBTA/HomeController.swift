3a224e6d7ae5f8a6272adb55401f0d7989dbb3d3
AdamWD <falco1016@gmail.com>
2018-02-14 11:28:47 +0800

    back up

---
+++ TwitterLBTA/HomeController.swift
@@ -0,0 +1,21 @@
+//
+//  HomeController.swift
+//  TwitterLBTA
+//
+//  Created by adamwang on 2018/2/14.
+//  Copyright © 2018年 adamwang. All rights reserved.
+//
+
+import UIKit
+
+class HomeController: UICollectionViewController{
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        collectionView?.backgroundColor = .green
+        
+        
+        
+    }
+}
+
+
